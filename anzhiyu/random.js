e3526512afe9a904288be1d60dfb4172f3d41288
YHYYDS666 <1768582979@qq.com>
2024-10-12 12:54:40 +0800

    Site updated: 2024-10-12 12:54:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/12/这是一篇新的博文/","2024/10/11/hello-world/"];function toRandomPost(){
+var posts=["2024/03/15/春天的味道/","2024/10/12/这是一篇新的博文/","2024/10/11/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
